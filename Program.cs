5dca3a2f20e04c97524aaf469f23b2a1c9e57380
Arrews <136616317+Arrews@users.noreply.github.com>
2023-06-14 18:09:39 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿Random r = new Random();
+
+int winNum = r.Next(0, 100);
+
+bool win = false;
+
+do
+{
+    Console.Write("Guess a number. Between 0-100:");
+    string s = Console.ReadLine();
+
+    int i = Convert.ToInt32(s);
+
+    if (i>winNum)
+    {
+        Console.WriteLine("Too high!");
+    }
+    else if (i<winNum)
+    {
+        Console.WriteLine("Too low!");
+    }
+    else if (i==winNum)
+    {
+        Console.WriteLine("That's the right number. Congratulations!");
+        win = true;
+    }
+
+    Console.WriteLine();
+
+} while (win == false);
+
+Console.WriteLine("Thank you for playing! Press any key to finish.");
+Console.ReadKey(true);
